1f0b2fb468b3e736ff2f2cbcc60f9d416006f1dd
Myth Yang <yangguangpkpk@gmail.com>
2016-03-30 17:57:42 +0800

    small change

---
+++ java_install.sh
@@ -0,0 +1,3 @@
+wget --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie;" http://download.oracle.com/otn-pub/java/jdk/8u73-b02/jdk-8u73-linux-x64.rpm
+rpm -ivh jdk-8u73-linux-x64.rpm
+rm -f jdk-8u73-linux-x64.rpm
